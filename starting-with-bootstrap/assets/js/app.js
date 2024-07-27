9536971c034a52e65d7304a01c2a96ffc95906b0
Yousef Elsrogy <90921892+joisyousef@users.noreply.github.com>
2024-07-27 15:26:04 +0300

    modifing files

---
+++ starting-with-bootstrap/assets/js/app.js
@@ -0,0 +1,226 @@
+// $(document).ready(function () {
+// //code
+//  $("#text-for-test")
+// });
+
+// $(function(){
+//     $("#text-for-test")
+// });
+
+$(document).ready(function () {
+  //   $(".text-for-test").dblclick(function(){
+  //       alert("Bye! You now leave p1!");
+  //     });
+  //   $(".text-for-test").mouseenter(function(){
+  //       alert("Bye! You now leave p1!");
+  //     });
+  // $(".text-for-test").mouseup(function(){
+  //     alert("Mouse up over p1!");
+  //   });
+
+  // $(".text-for-test").mousedown(function(){
+  //     alert("Mouse down over p1!");
+  //   });
+
+  // $(".text-for-test").hover(function(){
+  //     alert("You entered p1!");
+  // });
+
+  //   $("input").focus(function(){
+  //       $(this).css("border-color", "red");
+  //     //   $(this).css("background-color", "red");
+  //   });
+
+  //   $("input").blur(function(){
+  //     $(this).css("background-color", "red");
+  //   });
+  //   $(".text-for-test").on("click", function(){
+  //       $(this).hide();
+  //   });
+
+  // $("#hide").click(function () {
+  //   $("p").hide(1000);
+  // });
+  // $("#show").click(function () {
+  //   $("p").show(2000);
+  // });
+
+  // $("#toggle-button").click(function () {
+  //   $("p").toggle(3000);
+  // });
+
+  //   $("button").click(function(){
+  //     $("#div1").fadeTo("slow", 0.15);
+  //     $("#div2").fadeTo("slow", 0.4);
+  //     $("#div3").fadeTo("slow", 0.7);
+  //     });
+
+  // $("#flip").click(function(){
+  //     $("#panel").slideToggle("slow");
+  //   });
+
+  //   $("button").click(function(){
+  //     $("div").animate({
+  //         left: '250px',
+  //         opacity: '0.5',
+  //         height: '150px',
+  //         width: '150px'
+  //     });
+  //   });
+//   $("button").click(function () {
+//     $("div").animate({ height: "300px", opacity: "0.4" }, "slow");
+//     $("div").animate({ width: "300px", opacity: "0.8" }, "slow");
+//     $("div").animate({ height: "100px", opacity: "0.4" }, "slow");
+//     $("div").animate({ width: "100px", opacity: "0.8" }, "slow");
+//   });
+
+
+
+// $("button").click(function(){
+//     $("p").hide("slow", function(){
+//       alert("The paragraph is now hidden");
+//     });
+//   });
+
+// $("button").click(function(){
+//     $(".text-for-test").css("color", "red").slideUp(2000).slideDown(2000);
+//   });
+
+
+  // $("#flip").click(function(){
+  //     $("#panel").slideDown(5000);
+  //   });
+
+  //   $("#stop").click(function(){
+  //     $("#panel").stop();
+  //   });
+
+  //   $("button").click(function () {
+  //     $("p").hide(1000, function () {
+  //       alert("The paragraph is now hidden");
+  //     });
+  //   });
+
+  // $("p").css("color", "red").slideUp(2000).slideDown(2000);
+
+    // $("#btn1").click(function () {
+    //   alert($("p").text());
+    // });
+    // $("#btn2").click(function () {
+    //   alert($("p").html());
+    // });
+
+//   $("button").click(function(){
+//       alert($("input").val());
+//     });
+
+    // $("#btn1").click(function () {
+    //   $("#test1").text("Hello world!");
+    // });
+    // $("#btn2").click(function () {
+    //   $("#test2").html("<i class='italicFont'>Hello world!</i>");
+    // });
+    // $("#btn3").click(function () {
+    //   $("#test3").val("Dolly Duck");
+    // });
+
+//   $("button").click(function () {
+//     $("p,div,.text").hide(1000);
+//   });
+
+
+// $("#btn1").click(function(){
+//     $("p").append(" <b>Appended text</b>.");
+//   });
+
+//   $("#btn2").click(function(){
+//     $("ol").append("<li>Appended item</li>");
+//   });
+
+
+
+
+
+// $("#btn1").click(function(){
+//     $("p").prepend(" <b>Appended text</b>.");
+//   });
+
+//   $("#btn2").click(function(){
+//     $("ol").prepend("<li>Appended item</li>");
+//   });
+
+
+
+  // $("button, div").click(function () {
+  //     $("p").hide(1000);
+  //   });
+
+//   $("button").click(function(){
+//       $("h1, h2, p").addClass("blue");
+//       $("div").addClass("another-test");
+//     });
+
+
+//     $("p").css("background-color", "yellow");
+
+
+// $("span").parentsUntil('ul').css({"color": "red", "border": "2px solid red"});
+
+// $("div").children().css({"color": "red", "border": "2px solid red"});
+
+// $("div").find("span").css({"color": "red", "border": "2px solid red"});
+
+// $("h2").siblings().css({"color": "red", "border": "2px solid red"});
+
+// $("div").first().css("background-color", "yellow");
+
+  // $("button").click(function(){
+  //     $("h1, h2, p, div").addClass("important blue");
+  //   });
+
+  $( "#test-sorting-sec" ).sortable();
+
+  // $( "#accordion" ).accordion();
+
+  // $('.owl-carousel').owlCarousel({
+  //     loop:true,
+  //     margin:10,
+  //     responsiveClass:true,
+  //     dots:true,
+  //     autoplay:true,
+  //     rtl:true,
+  //     responsive:{
+  //         0:{
+  //             items:1,
+  //             nav:true
+  //         },
+  //         600:{
+  //             items:3,
+  //             nav:false
+  //         },
+  //         1000:{
+  //             items:5,
+  //             nav:true,
+  //             loop:false
+  //         }
+  //     }
+  // })
+
+
+//   $('.owl-carousel').owlCarousel({
+//     items:5,
+//     loop:true,
+//     margin:10,
+//     autoplay:true,
+//     autoplayTimeout:1000,
+//     autoplayHoverPause:true,
+//     responsive:{
+//         678:{
+//             mergeFit:true
+//         },
+//         1000:{
+//             mergeFit:false
+//         }
+//     }
+// });
+});
